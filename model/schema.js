2f7bef660b9778244234f95432ecc45f580af378
Gui7001br <Paulo Henrique>
2024-01-21 20:29:13 -0400

    Api

---
+++ model/schema.js
@@ -0,0 +1,37 @@
+const mongo = require("mongoose");
+const { Schema } = mongo;
+
+const userSchema = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    Telefone: {
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    Planodetreino:{
+        type:  String,
+        required: true
+    },
+    Instrutor:{
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+
+module.exports = mongo.model('User',userSchema);
